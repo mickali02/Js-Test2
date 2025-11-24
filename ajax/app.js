2e71989f3c5a9118328a28e70daceee60ce7cafe
mickali02 <2020151994@ub.edu.bz>
2025-11-24 12:52:20 -0600

    Inital commit

---
+++ ajax/app.js
@@ -0,0 +1,12 @@
+// Default values
+const defaultImage = "DEFAULT_IMAGE_URL_HERE";
+const defaultQuote = "DEFAULT QUOTE HERE";
+
+document.getElementById("generateBtn").addEventListener("click", () => {
+  // TODO:
+  // 1. Update status to "Loading poster..."
+  // 2. Fetch image from https://picsum.photos/800/400
+  // 3. Fetch quote from https://dummyjson.com/quotes/random
+  // 4. Update DOM with image + quote
+  // 5. Handle failures with defaults
+});
